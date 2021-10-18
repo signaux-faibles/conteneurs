2a811c691c7990398adf89336154463384252681
Raphaël Squelbut <raphael.squelbut@alibeez.com>
2021-10-18 12:43:55 +0200

    feat: ajoute un shebang dans le build de matomo et postgres

--- matomo/build.sh
+++ matomo/build.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 docker build -t sf-matomo .
 docker save sf-matomo | gzip > sf-matomo.tar.gz
--- postgres/build.sh
+++ postgres/build.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 docker build -t postgres .
 docker save postgres | gzip > postgres.tar.gz
