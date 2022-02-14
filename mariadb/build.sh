7749f461dc882a214aaf29f47d8c5a2eb4ebe3fa
Raphaël Squelbut <raphael.squelbut@alibeez.com>
2022-02-14 15:16:55 +0100

    feat(all): refine containers tags 5

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# dummy comment to just start new github action !
+# dummy comment to just start new github action !!
 
 docker build -t sf-mariadb .
 docker save sf-mariadb | gzip > sf-mariadb.tar.gz
